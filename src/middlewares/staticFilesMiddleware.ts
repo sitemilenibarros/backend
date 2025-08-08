e26e5f799c3dc747e30ec7bd0a3a07ba14365c41
João Pedro Lopes Moreno <joao.moreno@bomfuturo.com.br>
2025-08-08 18:13:41 -0400

    teste liberação cors

--- src/middlewares/staticFilesMiddleware.ts
+++ src/middlewares/staticFilesMiddleware.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import path from 'path';
+import cors from "cors";
 
 export const serveStaticFiles = (app: express.Application) => {
+    app.use(cors({
+        origin: '*'
+    }));
     app.use('/assets/services', express.static(path.join(__dirname, '../assets/services')));
     app.use('/assets/events', express.static(path.join(__dirname, '../assets/events')));
     app.use('/assets/testimonials', express.static(path.join(__dirname, '../assets/testimonials')));
