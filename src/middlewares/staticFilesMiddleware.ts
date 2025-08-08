c799fa2452bf3ee1409538e5ab983dea427b51ae
João Pedro Lopes Moreno <joao.moreno@bomfuturo.com.br>
2025-08-08 18:16:10 -0400

    teste liberação cors

--- src/middlewares/staticFilesMiddleware.ts
+++ src/middlewares/staticFilesMiddleware.ts
@@ -6,6 +6,10 @@ export const serveStaticFiles = (app: express.Application) => {
     app.use(cors({
         origin: '*'
     }));
+    app.use((req, res, next) => {
+        res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+        next();
+    });
     app.use('/assets/services', express.static(path.join(__dirname, '../assets/services')));
     app.use('/assets/events', express.static(path.join(__dirname, '../assets/events')));
     app.use('/assets/testimonials', express.static(path.join(__dirname, '../assets/testimonials')));
